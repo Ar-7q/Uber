33a6577de79bef059a3b63c92120f72757227a86
Ar-7q <arpitrai927@gmail.com>
2025-03-09 03:11:10 +0530

    adding the necessary files to the backend section

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,8 @@ const cookieparser=require('cookie-parser')
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.route');
 const captainRoutes=require('./routes/captain.routes')
+const mapRoutes=require('./routes/maps.routes')
+const rideRoutes=require('./routes/ride.routes')
 
 connectToDb();
 const app = exp();
@@ -22,6 +24,8 @@ app.get('/', (req, res) => {
 
 app.use('/user', userRoutes);
 app.use('/captain',captainRoutes)
+app.use('/maps',mapRoutes)
+app.use('/rides',rideRoutes)
 
 
 
